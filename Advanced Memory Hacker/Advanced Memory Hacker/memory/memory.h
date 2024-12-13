be98c5c9e709c109f272b29362fd163e1e7a18cc
Sproute <153927002+SprouteRL@users.noreply.github.com>
2024-12-13 23:04:05 +0200

    forgot to update whole thing

--- Advanced Memory Hacker/Advanced Memory Hacker/memory/memory.h
+++ Advanced Memory Hacker/Advanced Memory Hacker/memory/memory.h
@@ -57,49 +57,20 @@ public:
 	template <typename Ty>
 	bool WriteMemory(const uintptr_t& address, const Ty& buffer)
 	{
-		if (!InitializeNtdll()) {
-			std::cout << "init?" << "\n";
-			return false;
-		}
-		if (!this->handle) {
-			std::cout << "handle?" << "\n";
-			return false;
-		}
-
-		if (!IsMemoryOk(address)) {
-			std::cout << "is ok?" << "\n";
-			return false;
-		}
+		if (!InitializeNtdll()) return false;
+		if (!this->handle) return false;
+		if (address == 0) return false;
+		if (!IsMemoryOk(address)) return false;
 
 		return NtWriteVirtualMemory(handle, reinterpret_cast<LPVOID>(address), (PVOID)&buffer, sizeof(buffer), nullptr) == 0;
 	}
 	template <typename Ty>
 	bool WriteMemory(const uintptr_t& address, const Ty& buffer, size_t size)
 	{
-		std::cout << "hi\n";
-
-		if (!InitializeNtdll()) {
-			std::cerr << "Failed to initialize Ntdll!" << std::endl;
-			return false;
-		}
-
-		/*if (!this->handle) {
-			std::cerr << "Invalid process handle!" << std::endl;
-			return false;
-		}*/
-
-		if (address == 0) {
-			std::cerr << "Invalid address: 0x" << std::hex << address << std::dec << std::endl;
-			return false;
-		}
-
-		if (!IsMemoryOk(address)) {
-			std::cerr << "Memory at address 0x" << std::hex << address << std::dec << " is not ok!" << std::endl;
-			return false;
-		}
-
-		// Debugging: Print buffer content
-		std::cout << "Writing " << size << " bytes to memory at address 0x" << std::hex << address << std::dec << std::endl;
+		if (!InitializeNtdll()) return false;
+		if (!this->handle) return false;
+		if (address == 0) return false;
+		if (!IsMemoryOk(address)) return false;
 
 		return NtWriteVirtualMemory(handle, reinterpret_cast<LPVOID>(address), (PVOID)&buffer, size, nullptr) == 0;
 	}
